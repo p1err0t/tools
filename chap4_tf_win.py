bb1053f8bb14c973962029b455d5180362c0d8f7
young <kyzh@whu.edu.cn>
2019-05-30 17:16:55 +0800

    update

--- chap4_tf_win.py
+++ chap4_tf_win.py
@@ -51,5 +51,46 @@ class FunChap4:
                 print("After %s steps: global_step is %f, w is %f,learning rate is %f,"
                       " loss is %f." % (i,global_step_val, w_val,learning_rate_val,loss_val))
 
+    @staticmethod
+    def func4_3():
+        w1 = tf.Variable(0, dtype=tf.float32)
+        # 定义num_updates(NN的迭代轮数)，初始值为0，不可被优化
+        global_step = tf.Variable(0, trainable=False)
+        # 实例化滑动平均类，滑动平均衰减率为0.99
+        MOVING_AVERAGE_DECAY = 0.99
+        ema = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+        # 更新列表，求滑动平均值
+        ema_op = ema.apply(tf.trainable_variables())
+        # 查看不同迭代中变量取值的变化
+        with tf.Session() as sess:
+            init_op = tf.global_variables_initializer()
+            sess.run(init_op)
+            print(sess.run([w1, ema.average(w1)]))
 
-FunChap4.func4_2()
+            # 参数w1的值赋为1
+            sess.run(tf.assign(w1, 1))
+            sess.run(ema_op)
+            print(sess.run([w1, ema.average(w1)]))
+
+            # 更新step和w1的值，模拟出100轮迭代后，参数w1变为10
+            sess.run(tf.assign(global_step, 100))
+            sess.run(tf.assign(w1, 10))
+            sess.run(ema_op)
+            print(sess.run([w1, ema.average(w1)]))
+
+            sess.run(ema_op)
+            print(sess.run([w1, ema.average(w1)]))
+            sess.run(ema_op)
+            print(sess.run([w1, ema.average(w1)]))
+            sess.run(ema_op)
+            print(sess.run([w1, ema.average(w1)]))
+            sess.run(ema_op)
+            print(sess.run([w1, ema.average(w1)]))
+            sess.run(ema_op)
+            print(sess.run([w1, ema.average(w1)]))
+            sess.run(ema_op)
+            print(sess.run([w1, ema.average(w1)]))
+
+
+
+FunChap4.func4_3()
